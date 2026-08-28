04afd1f8152f61557d4eff9fa53f15727e45ffdb
pieroguevaragarrido <pieroestefanogg@outlook.com>
2026-08-28 17:12:52 -0500

    Calculos realizados

--- app/src/main/java/com/guevara/pagouniversidad/Main.kt
+++ app/src/main/java/com/guevara/pagouniversidad/Main.kt
@@ -12,6 +12,7 @@ fun main() {
 
     val nombresCursos = mutableListOf<String>()
     val creditosCursos = mutableListOf<Int>()
+    val costosCursos = mutableListOf<Double>()
 
     var i = 1
     while (i <= cantidadCursos) {
@@ -21,22 +22,47 @@ fun main() {
         println("Creditos:")
         val creditos = readLine()!!.toInt()
 
+        val costoCurso = creditos * valorCredito
+
         nombresCursos.add(nombreCurso)
         creditosCursos.add(creditos)
+        costosCursos.add(costoCurso)
 
         i++
     }
 
-    // Impresion temporal solo para confirmar que el input se capturo bien
-    println()
-    println("--- Datos capturados ---")
-    println("Estudiante: $nombreEstudiante")
-    println("Cantidad de cursos: $cantidadCursos")
-    println("Valor por credito: S/$valorCredito")
+    var totalCreditos = 0
+    var totalPagar = 0.0
 
     var j = 0
-    while (j < nombresCursos.size) {
-        println("Curso: ${nombresCursos[j]} - Creditos: ${creditosCursos[j]}")
+    while (j < creditosCursos.size) {
+        totalCreditos += creditosCursos[j]
+        totalPagar += costosCursos[j]
         j++
     }
+
+    var cargaAcademica = ""
+    if (totalCreditos <= 12) {
+        cargaAcademica = "M.R"
+    } else if (totalCreditos in 13..18) {
+        cargaAcademica = "Carga completa"
+    } else {
+        cargaAcademica = "Requiere autorizacion"
+    }
+
+    var numeroCuotas = 0
+    if (totalPagar > 2500) {
+        numeroCuotas = 3
+    } else {
+        numeroCuotas = 2
+    }
+    val valorCuota = totalPagar / numeroCuotas
+
+    println()
+    println("--- Calculos realizados ---")
+    println("Total de creditos: $totalCreditos")
+    println("Total a pagar: S/$totalPagar")
+    println("Carga academica: $cargaAcademica")
+    println("Numero de cuotas: $numeroCuotas")
+    println("Valor de cada cuota: S/$valorCuota")
 }
